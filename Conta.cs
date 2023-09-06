4b08390e07a7247bb792e4b4f820a47201593199
FBrandaoSilva <filipebrandao.dasilva@gmail.com>
2023-09-06 20:18:06 -0300

    Adicionar arquivos de projeto.

---
+++ Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleContas
+{
+    internal class Conta
+    {
+        //private int _numero;
+        private decimal _saldo;
+
+        public int Numero { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleContas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta1 = new Conta();
+            conta1.Numero = 123456;
+            Console.WriteLine(conta1.Numero);
+        }
+    }
+}
